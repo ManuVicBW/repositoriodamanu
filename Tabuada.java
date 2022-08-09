3629ca5495dde5a73faf783c523470acc7d0c5cc
ManuVicBW <110927918+ManuVicBW@users.noreply.github.com>
2022-08-09 14:14:40 -0300

    Add files via upload

---
+++ Tabuada.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etapa1;
+
+/**
+ *
+ * @author 202011640014
+ */
+public class Tabuada {
+
+    public static void main(String[] args) {
+        int tabuada = 5; // tabuada do 5
+        for (int x = 0; x <= 10; x++) {
+           
+            System.out.print("Tabuada do: ");
+   
+        
+        System.out.print(x + " x " + tabuada + " = ");
+        System.out.println(x * tabuada);
+        }
+    }
+}
